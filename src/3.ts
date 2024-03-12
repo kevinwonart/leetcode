eea02a1406311174f5f2721eb4a121d58eabb3f9
Kevin Won <kevin.won91@gmail.com>
2024-03-12 14:49:36 -0500

    problem 3

---
+++ src/3.ts
@@ -0,0 +1,57 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+/*3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+*/
+
+function lengthOfLongestSubstring(s: string): number {
+  const charMap = new Map();
+  let left = 0;
+  let window = 0;
+  const allUniqueChar = (charMap: Map<string, number>) => {
+    return Array.from(charMap.values()).every(value => value < 2);
+  };
+  for(let i = 0; i < s.length; i++) {
+    if(charMap.has(s[i]) && charMap.get(s[i]) >= left) {
+      left = charMap.get(s[i]) + 1;
+    }
+  window = Math.max(window, i - left + 1);
+  charMap.set(s[i], i);
+  }
+  return window;
+};
+
+let str1 = "abcabcbb";
+const str2 = "bbbbb";
+const str3 = "pwwkew";
+console.assert(lengthOfLongestSubstring(str1) === 3, `Expects 3 | output: ${lengthOfLongestSubstring(str1)} | String input \"${str1}\"`);
+console.assert(lengthOfLongestSubstring(str2) === 1, `Expects 1 | output: ${lengthOfLongestSubstring(str2)} | String input \"${str2}\"`);
+console.assert(lengthOfLongestSubstring(str1) === 3, `Expects 3 | output: ${lengthOfLongestSubstring(str3)} | String input \"${str3}\"`);
--- src/424.ts
+++ src/424.ts
@@ -22,25 +22,54 @@ There may exists other ways to achieve this answer too.
  *
 */
 
-function characterReplacement(s: string, k: number): number {
-  const isPassingAnswer = (subSet: Set<string>): boolean => {
-    if(subSet.size <= k + 1) {
-      return true;
-    }
-    return false;
-  }
-  let subString = [...s];
-  for(let i = 0; i < s.length; i++) {
-    let subString = [...s];
 
-    let subSet= new Set(subString);
-    if(isPassingAnswer(subSet)){
-      return s.length - i;
+
+function characterReplacement(s: string, k: number): number {
+  let window = 0;
+  let frequencyCount = 0;
+  let left = 0;
+  const hashMap: { [key:string]: number } = {};
+  for (let right = 0; right < s.length; right++) {
+    hashMap[s[right]] = (hashMap[s[right]] || 0) + 1;
+
+    frequencyCount = Math.max(frequencyCount, hashMap[s[right]])
+    if (frequencyCount + k < right - left + 1 ) {
+      hashMap[s[left]]--;
+      left++;
     }
-    subString = subString.pop();
+    window = Math.max(window, right - left + 1);
   }
-  return 0;
+
+  return window;
 };
+
+/* Top Solution
+const characterReplacement = (s: string, k: number): number => {
+  const hashMap: { [key: string]: number } = {}
+  let longest: number = 0
+  let maxFreq: number = 0
+  let leftPointer: number = 0
+
+  for (let rightPointer = 0; rightPointer < s.length; rightPointer++) {
+    // Increment the frequency count by 1 upon encountering a character
+    hashMap[s[rightPointer]] = (hashMap[s[rightPointer]] || 0) + 1
+
+    // Maximum frequency of any character encountered so far in the current window.
+    maxFreq = Math.max(maxFreq, hashMap[s[rightPointer]])
+
+    // Move the window from the left until reaching `k` replacements
+    if (maxFreq + k < rightPointer - leftPointer + 1) {
+      hashMap[s[leftPointer]]--
+      leftPointer++
+    }
+
+    // Calculate the longest repeating character
+    longest = Math.max(longest, rightPointer - leftPointer + 1)
+  }
+
+  return longest
+}
+*/
 
 const str1 = "ABAB";
 const k1 = 2;
@@ -49,6 +78,18 @@ const k2 = 1;
 console.assert(characterReplacement(str1, k1) === 4, `Expect 4 | Got ${characterReplacement(str1, k1)} | str1: ${str1} | k1 ${k1}`);
 console.assert(characterReplacement(str2, k2) === 4, `Expect 4 | Got ${characterReplacement(str2, k2)} | str2: ${str2} | k2 ${k2}`);
 
+/*
+const hashMap: { [key: string]: number } = {}
+let arr: string[] = ["a", "b", "c", "c", "b", "a"];
+hashMap[arr[0]] = (hashMap[arr[0]] || 0 ) + 1;
+hashMap[arr[0]] = (hashMap[arr[0]] || 0 ) + 1;
+hashMap[arr[1]] = (hashMap[arr[1]] || 0 ) + 1;
+console.log("here");
+console.log(hashMap);
+*/
+
+
 export {};
 
-//solution is unfinished
+//notes: i copied the solution. my first try at using sets doesnt work because it will then manipulate the data. if there's a repeat
+//sets are blind to a repeat so if you slide the window it will erase the data. not integrous.
