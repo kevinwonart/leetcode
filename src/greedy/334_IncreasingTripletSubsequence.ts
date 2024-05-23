82069e6ff3aa8bd29cc36690be3b0ef267ef01f5
kevin won <kevin.won91@gmail.com>
2024-05-23 12:13:49 -0500

    334 Increasing Triplet

--- src/greedy/334_IncreasingTripletSubsequence.ts
+++ src/greedy/334_IncreasingTripletSubsequence.ts
@@ -28,8 +28,38 @@
   -231 <= nums[i] <= 231 - 1
  */
 
+function increasingTriplet(nums: number[]): boolean {
+  let left = 0
+  let right = nums.length
+  let min = Infinity
+  let max = Infinity
+
+  while (nums[left] > nums[left - 1]) left++;
+  while (nums[right] < nums[right - 1]) right--;
+
+  for (left; left < right; left++) {
+    if (nums[left] <= min) {
+      min = nums[left]
+    } else if (nums[left] <= max) {
+      max = nums[left]
+    } else {
+      return true
+    }
+  }
+  return false
+}
+
+const input1 = [1, 2, 3, 4, 5]
+const output1 = true
+const input2 = [5, 4, 3, 2, 1]
+const output2 = false
+const input3 = [2, 1, 5, 0, 4, 6]
+const output3 = true
+
+console.assert(increasingTriplet(input1) === output1, `Expected ${output1} | Received ${increasingTriplet(input1)} | Input ${input1}`)
+console.assert(increasingTriplet(input2) === output2, `Expected ${output2} | Received ${increasingTriplet(input2)} | Input ${input2}`)
+console.assert(increasingTriplet(input3) === output3, `Expected ${output3} | Received ${increasingTriplet(input3)} | Input ${input3}`)
 /* Summary:
- *
  *
  */
 export { }
